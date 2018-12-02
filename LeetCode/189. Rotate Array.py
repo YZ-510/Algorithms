9458470d0cbe5063d8a58fb32af6c4f8750ec23b
YZ-510 <yinzhe510@163.com>
2018-12-02 17:03:52 +0800

    Rotate Array

---
+++ LeetCode/189. Rotate Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        # k 如果大于 nums 的长度了，只需进行 k % len(nums) 次就行了
+        k = k % len(nums)
+        if k != 0:
+            temp = nums[-k:]
+            for i in range(len(nums)-1, k-1, -1):
+                nums[i] = nums[i-k]
+            nums[:k] = temp
