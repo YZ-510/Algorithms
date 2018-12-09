a51c7626e112b82af3d2b691df909520458e20db
YZ-510 <yinzhe510@163.com>
2018-12-09 22:07:43 +0800

    Valid Palindrome

---
+++ LeetCode/125. Valid Palindrome.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # 双指针
+        m, n = 0, len(s)-1
+        while m < n:
+            while m < n and not s[m].isalnum():
+                m += 1
+            while m < n and not s[n].isalnum():
+                n -= 1
+            if s[m].lower() != s[n].lower():
+                return False
+            m += 1
+            n -= 1
+        return True
+        
+        
+        '''
+        s = ''.join(i for i in s if i.isalnum()).lower()
+        return s == s[::-1]
+        
+        
+        n = ''
+        s = s.lower()
+        for i in s:
+            if '0' <= i <= '9' or 'a' <= i <= 'z':
+                n = n + i
+                
+        return n == n[::-1]
+        '''
