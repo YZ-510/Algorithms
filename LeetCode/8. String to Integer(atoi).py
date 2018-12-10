16836a39580af9dfe18d2b0f5ceb8d3dbfac87d0
YZ-510 <yinzhe510@163.com>
2018-12-10 16:19:24 +0800

    String to Integer(atoi)

---
+++ LeetCode/8. String to Integer(atoi).py
@@ -0,0 +1,41 @@
+class Solution:
+    def myAtoi(self, str):
+        """
+        :type str: str
+        :rtype: int
+        """
+        str = str.strip()
+        n = ''
+        positive = True
+        if str == '':
+            return 0
+        if str[0] == '+' or str[0] == '-':
+            if str[0] == '-':
+                positive = False
+            str = str[1:]
+        for char in str:
+            if '0' <= char <= '9':
+                n = n + char
+            else:
+                break                
+        '''
+        n = 0
+        for char in str:
+            if '0' <= char <= '9':
+                n = n * 10 + ord(char) - ord('0')
+            else:
+                break
+        '''
+        
+        if n == '':
+            return 0
+        else:
+            n = int(n)
+        if n > 2147483647:
+            if positive == False:
+                return -2147483648
+            else:
+                return 2147483647
+        if not positive:
+            return 0 - n
+        return n
