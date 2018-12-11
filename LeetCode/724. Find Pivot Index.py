bd0d3477de3818d1dfa44e6757bfc4b142848f5b
YZ-510 <yinzhe510@163.com>
2018-12-11 21:51:02 +0800

    Find Pivot Index

---
+++ LeetCode/724. Find Pivot Index.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left_sum, right_sum = 0, sum(nums)
+        for idx, num in enumerate(nums):
+            right_sum -= num
+            if left_sum == right_sum:
+                return idx
+            left_sum += num
+        return -1
