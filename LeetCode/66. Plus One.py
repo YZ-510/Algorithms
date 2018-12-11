bab8ee0016292d1dfbfb4153d5669bdd03acaa41
YZ-510 <yinzhe510@163.com>
2018-12-11 21:50:41 +0800

    Plus One

--- LeetCode/66. Plus One.py
+++ LeetCode/66. Plus One.py
@@ -1,9 +1,21 @@
-class Solution:
+class Solution(object):
     def plusOne(self, digits):
         """
         :type digits: List[int]
         :rtype: List[int]
         """
+        # 倒序逐个判断当前值是否小于 9
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+        if i == 0:
+            digits.insert(0, 1)
+        return digits
+
+        '''
         i = len(digits) - 1
         while digits[i] == 9 and i >= 0:
             digits[i] = 0
@@ -13,3 +25,4 @@ class Solution:
         else:
             digits[i] = digits[i] + 1
         return digits
+        '''
