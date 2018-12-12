5b82f6f5981b63b7b657bf2fb4ffd67a44050d64
YZ-510 <yinzhe510@163.com>
2018-12-12 19:56:55 +0800

    Diagonal Traverse

---
+++ LeetCode/498. Diagonal Traverse.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def findDiagonalOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        if matrix == []:
+            return []
+        
+        m = len(matrix)
+        n = len(matrix[0])
+        i = 0
+        j = 0
+        result = []
+        
+        for k in range(m * n):
+            result.append(matrix[i][j])
+            # 判断奇偶
+            if (i + j) % 2 == 0:
+                # 判断边界
+                if j == n -1:
+                    i += 1
+                elif i == 0:
+                    j += 1
+                else:
+                    i -= 1
+                    j += 1
+            else:
+                # 判断边界
+                if i == m-1:
+                    j += 1
+                elif j == 0:
+                    i += 1
+                else:
+                    i += 1
+                    j -= 1
+                    
+        return result
