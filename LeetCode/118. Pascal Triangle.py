3be96a76ed61b88951d53f17050a760499b62070
YZ-510 <yinzhe510@163.com>
2018-12-12 20:00:21 +0800

    Pascal's Triangle

---
+++ LeetCode/118. Pascal Triangle.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        
+        for i in range(numRows):
+            if i == 0:
+                temp = [1]
+            else:
+                temp = []
+                temp.append(1)
+                for j in range(1, i):
+                    num = result[i-1][j] + result[i-1][j-1]
+                    temp.append(num)
+                temp.append(1)
+            result.append(temp)
+            
+        return result
