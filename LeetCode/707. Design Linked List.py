493e4eb5fa6bdc8385db44698128dee60bd03066
YZ-510 <yinzhe510@163.com>
2018-12-16 22:11:25 +0800

    设计链表

---
+++ LeetCode/707. Design Linked List.py
@@ -0,0 +1,68 @@
+# 设计链表
+# https://leetcode-cn.com/problems/design-linked-list/
+
+class MyLinkedList(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.lst = []
+
+    def get(self, index):
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        :type index: int
+        :rtype: int
+        """
+        if index > len(self.lst) - 1 or index < 0:
+            return -1
+        return self.lst[index]
+
+    def addAtHead(self, val):
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        self.lst.insert(0, val)
+
+    def addAtTail(self, val):
+        """
+        Append a node of value val to the last element of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        self.lst.append(val)
+
+    def addAtIndex(self, index, val):
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        :type index: int
+        :type val: int
+        :rtype: void
+        """
+        if index > len(self.lst):
+            return
+        elif index == len(self.lst):
+            self.lst.append(val)
+        else:
+            self.lst.insert(index, val)
+
+    def deleteAtIndex(self, index):
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        :type index: int
+        :rtype: void
+        """
+        if 0 <= index < len(self.lst):
+            self.lst.pop(index)
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
