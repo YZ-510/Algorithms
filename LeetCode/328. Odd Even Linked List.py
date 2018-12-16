91c429128018c5e42a3424e7ac11e79df5351b5e
YZ-510 <yinzhe510@163.com>
2018-12-16 22:11:01 +0800

    奇偶链表

---
+++ LeetCode/328. Odd Even Linked List.py
@@ -0,0 +1,30 @@
+# 奇偶链表
+# https://leetcode-cn.com/problems/odd-even-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None:
+            return head
+        
+        odd = oddHead = head
+        even = evenHead = head.next
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            
+            even.next = odd.next
+            even = even.next
+            
+        odd.next = evenHead
+        
+        return head
