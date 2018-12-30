796630186da97045afb708366676612e10b3e9bb
YZ-510 <yinzhe510@163.com>
2018-12-30 22:30:50 +0800

    两个列表的最小索引总和

---
+++ LeetCode/599. Minimum Index Sum of Two Lists.py
@@ -0,0 +1,30 @@
+# 两个列表的最小索引和
+# https://leetcode-cn.com/problems/minimum-index-sum-of-two-lists/
+
+class Solution(object):
+    def findRestaurant(self, list1, list2):
+        """
+        :type list1: List[str]
+        :type list2: List[str]
+        :rtype: List[str]
+        """
+        dic = {}
+        res = []
+        index = float("inf")
+        
+        for i in range(len(list1)):
+            dic[list1[i]] = i
+            
+        for j in range(len(list2)):
+            if list2[j] in dic:
+                if index > j + dic[list2[j]]:
+                    if res == []:
+                        res.append(list2[j])
+                    else:
+                        res.pop(-1)
+                        res.append(list2[j])
+                elif index == j + dic[list2[j]]:
+                    res.append(list2[j])
+                index = j + dic[list2[j]]
+                
+        return res
