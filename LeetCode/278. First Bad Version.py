e7703a96b910470450020518fc87092e338272c4
YZ-510 <yinzhe510@163.com>
2019-01-02 21:52:16 +0800

    第一个错误的版本

---
+++ LeetCode/278. First Bad Version.py
@@ -0,0 +1,23 @@
+# 第一个错误的版本
+# https://leetcode-cn.com/problems/first-bad-version/
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left = 1
+        right = n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if not isBadVersion(mid):
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
