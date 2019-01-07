0f09f2d5d9797cdc0143bd7f2640195fb2c2a210
YZ-510 <yinzhe510@163.com>
2019-01-07 22:13:14 +0800

    从前序与中序遍历序列构造二叉树

---
+++ LeetCode/105. Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,24 @@
+# 从前序与中序遍历序列构造二叉树
+# https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder or len(preorder) == 0:
+            return None
+        root = TreeNode(preorder[0])
+        idx = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:idx+1], inorder[0:idx])
+        root.right = self.buildTree(preorder[idx+1:], inorder[idx+1:])
+        return root
