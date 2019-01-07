30d59098e064fe85e465fe49fbdc04ae9ffd7fd0
YZ-510 <yinzhe510@163.com>
2019-01-07 22:12:32 +0800

    二叉树的最大深度

---
+++ LeetCode/104. Maximum Depth of Binary Tree.py
@@ -0,0 +1,35 @@
+# 二叉树的深度
+# https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        left_height = self.maxDepth(root.left)
+        right_height = self.maxDepth(root.right)
+        return max(left_height, right_height) + 1        
+        
+        '''
+        if not root:
+            return 0
+        left_height, right_height = 1, 1
+        if root.left:
+            left_height += self.maxDepth(root.left)
+        if root.right:
+            right_height += self.maxDepth(root.right)
+        if left_height > right_height:
+            return left_height
+        else:
+            return right_height
+        '''
