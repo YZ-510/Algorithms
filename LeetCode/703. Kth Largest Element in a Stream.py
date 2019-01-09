c1b441bad11c2ca2a1ca84d2ce8e5bd36d1a928b
YZ-510 <yinzhe510@163.com>
2019-01-09 21:09:59 +0800

    数据流中的第 K 大元素

---
+++ LeetCode/703. Kth Largest Element in a Stream.py
@@ -0,0 +1,36 @@
+# 数据流中的第 K 大元素
+# https://leetcode-cn.com/problems/kth-largest-element-in-a-stream/
+
+class KthLargest(object):
+    
+    # 维护一个大小为 K 的堆，保存从第 K 大的数字到最大的数字
+    
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.pool = nums
+        self.size = len(self.pool)
+        self.k = k
+        heapq.heapify(self.pool)
+        while self.size > k:
+            heapq.heappop(self.pool)
+            self.size -= 1
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        if self.size < self.k:
+            heapq.heappush(self.pool, val)
+            self.size += 1
+        elif val > self.pool[0]:
+            heapq.heapreplace(self.pool, val)
+        return self.pool[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
