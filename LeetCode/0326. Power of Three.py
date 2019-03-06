78eddd08c71436413363925249108e6623ad86e2
YZ-510 <yinzhe510@163.com>
2019-03-06 16:21:42 +0800

    3的幂

---
+++ LeetCode/0326. Power of Three.py
@@ -0,0 +1,16 @@
+# 3的幂
+# https://leetcode-cn.com/problems/power-of-three/
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n < 1:
+            return False
+        while n > 1:
+            if n % 3 != 0:
+                return False
+            n /= 3
+        return True
